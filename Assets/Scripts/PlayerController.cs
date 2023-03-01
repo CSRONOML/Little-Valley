38f91db38208ecfa855603b0ce7fad95f56bd43f
Hérmeson <hermeson.freitas@yahoo.com>
2023-02-28 21:19:22 -0300

    Fix player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,18 +3,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     [SerializeField] private new Rigidbody2D rigidbody2D;
-    [SerializeField] private float walkSpeed;
-    [SerializeField] private float runSpeed;
+    [SerializeField] private float speed;
     [SerializeField] private float damp;
     
     public Vector2 Velocity { private set; get; }
 
     private Vector2 Acceleration;
 
-    private void Update() {
+    private void FixedUpdate() {
         var target = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
         Velocity = Vector2.SmoothDamp(Velocity, target, ref Acceleration, damp);
-        var speed = Input.GetButtonDown("Run") ? runSpeed : walkSpeed;
         rigidbody2D.MovePosition(rigidbody2D.position + Velocity * speed * Time.fixedDeltaTime);
     }
 
