cba62565b3e867b0f1826eb7eb164759e4fecaa5
Hérmeson <hermeson.freitas@yahoo.com>
2023-03-01 00:41:11 -0300

    Add shopkeeper button animation

---
+++ Assets/Scripts/Shopkeeper/ShopkeeperTrigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+public class ShopkeeperTrigger : MonoBehaviour {
+    
+    [Header("Settings")]
+    [SerializeField] private float speed;
+    
+    [Header("References")]
+    [SerializeField] private CanvasGroup shopkeeperOverlay;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        StartCoroutine(buttonTransitionRoutiune(1));
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        StartCoroutine(buttonTransitionRoutiune(0));
+    }
+
+    private IEnumerator buttonTransitionRoutiune(float target) {
+        for (var time = 1 - target; time != target; time = Mathf.MoveTowards(time, target, speed * Time.deltaTime)) {
+            shopkeeperOverlay.alpha = time;
+            yield return null;
+        }
+
+        shopkeeperOverlay.alpha = target;
+    }
+
+}
